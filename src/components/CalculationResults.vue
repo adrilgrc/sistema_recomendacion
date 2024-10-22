0b8a6f9eaa324fa8236b71419e1cd4bfc83e8ef1
Mariajose Zuloeta Brito <91660658+majozb@users.noreply.github.com>
2024-10-22 18:26:45 +0000

    feat: add SimplePrediction function

--- src/components/CalculationResults.vue
+++ src/components/CalculationResults.vue
@@ -4,9 +4,14 @@
     <p>Métrica seleccionada: <strong>{{ selectedMetric }}</strong></p>
 
     <div v-if="fileContent">
+      <!-- Muestra el componente para calcular y mostrar la matriz de similitud -->
       <component :is="currentComponent" :content="fileContent" @matrixComputed="updateMatrices" />
+
+      <!-- Muestra el componente para calcular las predicciones después de la matriz de similitud -->
+      <div v-if="utilityMatrix.length && similarityMatrix.length && selectedPredictionType ==='simple'">
+        <SimplePrediction :utilityMatrix="utilityMatrix" :similarityMatrix="similarityMatrix" :numNeighbors="numNeighbors" />
+      </div>
     </div>
-  
 
     <div v-else>
       <p>No hay contenido del archivo disponible.</p>
@@ -18,6 +23,7 @@
 import PearsonCorrelation from '@/components/CorrelationPearson.vue';
 import CosineDistance from '@/components/CosineDistance.vue';
 import EuclideanDistance from '@/components/EuclideanDistance.vue';
+import SimplePrediction from '@/components/SimplePrediction.vue';
 
 export default {
   props: {
@@ -28,12 +34,20 @@ export default {
     fileContent: {
       type: String,
       default: ''
+    },
+    numNeighbors: {
+      type: Number,
+      required: true
+    },
+    selectedPredictionType: {
+      type: String,
+      required: true
     }
   },
   data() {
     return {
       utilityMatrix: [],
-      pearsonMatrix: []
+      similarityMatrix: []
     };
   },
   computed: {
@@ -53,12 +67,27 @@ export default {
   components: {
     PearsonCorrelation,
     CosineDistance,
-    EuclideanDistance
+    EuclideanDistance,
+    SimplePrediction
   },
   methods: {
-    updateMatrices({ utilityMatrix, pearsonMatrix }) {
+    updateMatrices({ utilityMatrix, pearsonMatrix, cosineMatrix, euclideanMatrix }) {
       this.utilityMatrix = utilityMatrix;
-      this.pearsonMatrix = pearsonMatrix;
+      switch (this.selectedMetric) {
+        case 'pearson':
+            this.similarityMatrix = pearsonMatrix;
+            break;
+          case 'cosine':
+            this.similarityMatrix = cosineMatrix;
+            break;
+          case 'euclidean':
+            this.similarityMatrix = euclideanMatrix;
+            break;
+          default:
+            this.similarityMatrix = [];
+      }
+
+      this.$emit('matrixUpdated', { utilityMatrix: this.utilityMatrix, similarityMatrix: this.similarityMatrix });
     }
   }
 };
---
+++ src/components/SimplePrediction.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <h4 v-if="flag">No hay vecinos</h4>
+    <h4>Predicción Simple</h4>
+    <div v-if="prediction.length">
+      <table>
+        <thead>
+          <tr>
+            <th>Usuario/Ítem</th>
+            <!-- Generar encabezados dinámicamente para los ítems -->
+            <th v-for="(itemPred, itemIndex) in prediction[0]" :key="itemIndex">Ítem {{ itemIndex + 1 }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- Filas para cada usuario -->
+          <tr v-for="(userPredictions, userIndex) in prediction" :key="userIndex">
+            <td>Usuario {{ userIndex + 1 }}</td>
+            <!-- Celdas para cada predicción de ítem -->
+            <td v-for="(itemPred, itemIndex) in userPredictions" :key="itemIndex">
+              {{ isNumeric(itemPred) ? itemPred.toFixed(4) : 'N/A' }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <p>No hay predicciones disponibles.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    numNeighbors: {
+      type: Number,
+      required: true
+    },
+    utilityMatrix: {
+      type: Array,
+      required: true
+    },
+		similarityMatrix: {
+			type: Array,
+			required: true
+		}
+	},
+	data() {
+    return {
+      prediction: [],
+      flag: false
+    };	
+	},
+	mounted() {
+    this.calculateSimplePrediction();
+	},
+	methods: {
+    calculateSimplePrediction() {
+      if (this.numNeighbors <= 0 || !this.isNumeric(this.numNeighbors) || this.numNeighbors > this.utilityMatrix.length - 1) {
+        // this.flag = ("El número de vecinos debe ser mayor a 0.");
+        this.flag = true;
+        return null;
+      }
+			// console.log("Matriz de utilidad: ");
+			// console.table(this.utilityMatrix);
+			// console.log("Matriz de similitud: ");
+			// console.table(this.similarityMatrix);
+			// console.log("Vecinos: ", this.numNeighbors);
+			const numUsers = this.utilityMatrix.length;
+			const numItems = this.utilityMatrix[0].length;
+			const prediction = Array.from({ length: numUsers }, () => Array(numItems).fill(0));
+
+			for (let i = 0; i < numUsers; i++) {
+				for (let j = 0; j < numItems; j++) {
+          // Si el usuario ya ha calificado el ítem, no es necesario predecir
+          if (this.utilityMatrix[i][j] !== null) {
+            prediction[i][j] = this.utilityMatrix[i][j];
+            continue;
+          }
+					const neighbors = this.similarityMatrix[i]
+						.map((similarity, u) => ({ similarity, user: u }))
+						.filter(({ similarity, user }) => similarity !== null && i !== user) 
+						.sort((a, b) => b.similarity - a.similarity)
+						.slice(0, this.numNeighbors);
+
+					const sumSimilarities = neighbors.reduce((acc, { similarity }) => acc + Math.abs(similarity), 0);
+					const sumRatings = neighbors.reduce((acc, { similarity, user }) => acc + similarity * this.utilityMatrix[user][j], 0);
+
+					prediction[i][j] = sumSimilarities !== 0 ? sumRatings / sumSimilarities : 0;
+
+				}
+			}
+
+			this.prediction = prediction;
+			// console.log("Predicciones: ");
+			// console.table(prediction);
+		},
+		isNumeric(value) {
+      return !isNaN(parseFloat(value)) && isFinite(value);
+    }
+	}
+};
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 15px;
+}
+
+th, td {
+  border: 1px solid #dee2e6;
+  padding: 8px;
+  text-align: center;
+}
+</style>
