715f9776f168d96f839d94b8a15794331fa7fa9d
Samuel Lorenzo Sanchez <68753976+SamLorenzoSanc@users.noreply.github.com>
2024-10-28 14:26:26 +0000

    [Fixed]: FileContent Update, Introduce rows empty and columns empty in the file, and define a logic secuence in the formdata

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="app">
     <h1>Bienvenido al sistema de recomendación App</h1>
-
     <div class="content-container">
       <RecommendationMenu @updateMetrics="updateMetrics" />
       <FileReader @fileContentLoaded="updateFileContent" />
     </div>
-
-    <CalculationResults 
-      v-if="selectedMetric || selectedPredictionType || fileContent" 
-      :selectedMetric="selectedMetric" 
-      :selectedPredictionType="selectedPredictionType" 
-      :fileContent="fileContent" 
-      :numNeighbors="numNeighbors" 
+    <CalculationResults
+      v-if="selectedMetric || selectedPredictionType || fileContent"
+      :key="fileContent"
+      :selectedMetric="selectedMetric"
+      :selectedPredictionType="selectedPredictionType"
+      :fileContent="fileContent"
+      :numNeighbors="numNeighbors"
     />
   </div>
 </template>
 
 <script>
-
-// Importamos los componentes
 import FileReader from './components/FileReader.vue';
 import RecommendationMenu from './components/RecommendationMenu.vue';
 import CalculationResults from './components/CalculationResults.vue';
@@ -50,8 +47,35 @@ export default {
       // Actualiza el contenido del archivo
       this.fileContent = content;
     },
+    recalculateResults() {
+      // Lógica para recalcular resultados, por ejemplo, actualizar valores o hacer nuevas peticiones
+      console.log('Recalculando resultados con nuevas métricas, tipo de predicción o número de vecinos');
+
+    }
   },
+  watch: {
+  selectedMetric(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      this.recalculateResults();
+    }
+  },
+  selectedPredictionType(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      this.recalculateResults();
+    }
+  },
+  numNeighbors(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      this.recalculateResults();
+    }
+  },
+  fileContent(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      this.recalculateResults();
+    }
+  }
+  }
 };
 </script>
 
-<style src="@/styles/style.css"></style> <!-- Importa el CSS global -->
+<style src="@/styles/style.css"></style>
